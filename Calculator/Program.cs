47669eb8fccf5cbe868b46615b75a7183dae66e8
majkel1987 <m.sarnaki@icloud.com>
2023-01-31 18:44:16 +0100

    Add project files

---
+++ Calculator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Podaj pierwszą liczbę:");
+                var number1 = GetInput();
+
+                Console.WriteLine("Podaj typ działania: '+', '-', '/', '*'.");
+                var action = Console.ReadLine();
+
+                Console.WriteLine("Podaj drugą liczbę:");
+                var number2 = GetInput();
+
+                var result = Calculate(number1, number2, action);
+
+                Console.WriteLine("Wynik twojego działania to: " + result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+        private static int GetInput()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int input))
+                throw new Exception("Podana wartość jest nieprawidłowa.");
+            return input;
+        }
+        private static int Calculate(int number1, int number2, string action)
+        {
+            switch (action)
+            {
+                case "+":
+                    return number1 + number2;
+                case "-":
+                    return number1 - number2;
+                case "/":
+                    return number1 / number2;
+                case "*":
+                    return number1 * number2;
+                default:
+                    throw new Exception("Wybrałeś zły symbol!");
+            }
+            
+        }
+
+    }
+}
